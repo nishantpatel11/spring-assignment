06d927c2ba7379b262c6fd5e1cc78de3ee6a0f78
nishant patel <nishantpatelbca@gmail.comgit config --global user.name nishantpatel11>
2019-10-22 15:41:27 +0530

    Added employee

---
+++ src/main/java/com/nishu/service/impl/MyEmployeeService.java
@@ -0,0 +1,34 @@
+package com.nishu.service.impl;
+
+import com.nishu.bank.model.Employee;
+
+public class MyEmployeeService {
+
+	private Employee employee;
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public MyEmployeeService(){
+		System.out.println("MyEmployeeService no-args constructor called");
+	}
+
+	//pre-destroy method
+	public void destroy() throws Exception {
+		System.out.println("MyEmployeeService Closing resources");
+	}
+
+	//post-init method
+	public void init() throws Exception {
+		System.out.println("MyEmployeeService initializing to dummy value");
+		if(employee.getName() == null){
+			employee.setName("Pankaj");
+		}
+	}
+
+}
